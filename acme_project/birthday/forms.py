1bc93f668ea69500fa0dfec05d52390720d5a2a9
Ефим Шулепов <estelliar1@yandex.ru>
2023-08-22 16:04:57 +0300

    Sprint - 7. Topic - 4. Lesson - 2

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -15,22 +15,22 @@ class BirthdayForm(forms.ModelForm):
             'birthday': forms.DateInput(attrs={'type': 'date'})
         }
 
-        def clean_first_name(self):
-            first_name = self.cleaned_data['first_name']
-            return first_name.split()[0]
+    def clean_first_name(self):
+        first_name = self.cleaned_data['first_name']
+        return first_name.split()[0]
 
-        def clean(self):
-            super().clean()
-            first_name = self.cleaned_data['first_name']
-            last_name = self.cleaned_data['last_name']
-            if f'{first_name} {last_name}' in BEATLES:
-                send_mail(
-                    subject='Another Beatles member',
-                    message=f'{first_name} {last_name} пытался опубликовать запись!',
-                    from_email='birthday_form@acme.not',
-                    recipient_list=['admin@acme.not'],
-                    fail_silently=True,
-                )
-                raise ValidationError(
-                    'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
-                )
+    def clean(self):
+        super().clean()
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        if f'{first_name} {last_name}' in BEATLES:
+            send_mail(
+                subject='Another Beatles member',
+                message=f'{first_name} {last_name} пытался опубликовать запись!',
+                from_email='birthday_form@acme.not',
+                recipient_list=['admin@acme.not'],
+                fail_silently=True,
+            )
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
